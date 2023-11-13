6ae86e1cce7feb394bb875de454aae318fc0a902
Yevhenii <eugeneiv1@gmail.com>
2023-11-13 23:42:00 +0200

    add dar/light mode switch

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,13 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {FC, useState} from "react";
+
+import * as React from 'react';
+
+import IconButton from '@mui/material/IconButton';
+import { useTheme} from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { ColorModeContext } from "../../layuots/MainLayout";
 
 import {GenresComponent} from "../GenresContainer/GenresComponent/GenresComponent";
 import css from './Header.module.css'
@@ -11,6 +19,10 @@ interface IProps {
 }
 const Header:FC<IProps> = ({onSearchChange, show}) => {
     const [hide, setHide] = useState<boolean>(true);
+    const navigate = useNavigate()
+
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
     const toggleHide = ():void => {
         setHide(!hide);
     }
@@ -18,13 +30,17 @@ const Header:FC<IProps> = ({onSearchChange, show}) => {
     return (
         <div className={css.headerWrapper}>
             <div className={css.Header}>
-                <NavLink to={'../movies'}>The MovieDB</NavLink>
+                <p onClick={() => navigate('../movies')}>The MovieDB</p>
                 <ul className={css.headerList}>
-                    <li><NavLink to={'../movies'}>Movies</NavLink></li>
+                    <li><p onClick={() => navigate('../movies')}>Movies</p></li>
                     <li onClick={toggleHide}><p>Genres</p></li>
                     {show && <li><SearchComponent onSearchChange={onSearchChange}/></li>}
                 </ul>
                 <div>The MovieDB</div>
+                {theme.palette.mode} mode
+                <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+                    {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                </IconButton>
             </div>
             <div className={hide? css.genresWrapper : [css.genresWrapper, css.show].join(' ')} id={'genres'}>
                 <GenresComponent />
--- src/layuots/MainLayout.tsx
+++ src/layuots/MainLayout.tsx
@@ -1,8 +1,33 @@
 import {Outlet} from "react-router-dom";
+import * as React from 'react';
+
+import {ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
 import 'normalize.css';
 
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 const MainLayout = () => {
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+
 
     const hideGenres = (ev: { stopPropagation: () => void }) => {
         ev.stopPropagation();
@@ -13,12 +38,30 @@ const MainLayout = () => {
     }
 
     return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        bgcolor: 'background.default',
+                        color: 'text.primary',
+                        borderRadius: 1,
+                        p: 3,
+                    }}
+                >
             <div onClick={hideGenres}>
                 <Outlet/>
             </div>
+                </Box>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+
     );
 };
 
 export {
-    MainLayout
+    MainLayout,
+    ColorModeContext
 };
